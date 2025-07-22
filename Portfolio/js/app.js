d2165ece094883fe97a962c992ae9df9a5bcf515
Fayzee1406 <fayzulloabduqodirov765@gmail.com>
2025-07-22 14:44:52 +0500

    my-portfolio-site
    This is a clean and minimalist personal portfolio website built using only HTML, CSS, and vanilla JavaScript. The layout is fully responsive and optimized for both desktop and mobile devices. The design uses a black-and-white theme with a focus on simplicity, readability, and smooth navigation. This project showcases my frontend skills without relying on any external frameworks.

---
+++ Portfolio/js/app.js
@@ -0,0 +1,15 @@
+  /*navbar */
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+  var typed = new Typed(".text",{
+    strings:["Web Developer", "Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+     backDelay: 1000,
+    loop: true
+
+  });
